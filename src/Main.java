8c9bb883814e10d04a7a28e91f82283f10953a0d
Evinkar <andreylukyanovx@gmail.com>
2022-02-06 00:48:37 +0400

    Задание 1 Реализуйте метод public String collectBirthdays(int year, int month, int day), который вернёт строки, содержащие все ваши прошедшие дни рождения от нуля лет до текущей даты в требуемом формате. Дата рождения передаётся в метод тремя числами. Формат требуемого вывода: 0 - 31.12.1990 - Mon 1 - 31.12.1991 - Tue
    Задание 2
    public String getPeriodFromBirthday();
    таким образом, чтобы он возвращал количество лет, месяцев и дней с момента основания Java до сегодняшнего дня. Пример, формата вывода:
    
    years:26, months:5, days:9

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,46 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
+    public static LocalDate today = LocalDate.now();
+    public static LocalDate birthday;
+    public static int count = 0;
+    public static LocalDateTime javaBirthday;
+
+
     public static void main(String[] args) {
+        System.out.println(collectBirthdays(1989,03,30));
+        System.out.println(getPeriodFromBirthday());
+
 
     }
 
-    public String collectBirthdays(int year, int month, int day) {
-        return null;
+    public static String collectBirthdays(int year, int month, int day) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern ("dd.MM.yyyy - EEEE");
+        birthday = LocalDate.of(year, month, day);
+        String text = "";
+        do{
+            //System.out.println(count + " - " + formatter.format(birthday));
+            text = text + count + " - " + formatter.format(birthday) + System.lineSeparator();
+            birthday = birthday.plusYears(1);
+            count = count + 1;
+
+        } while (birthday.isBefore(today));
+
+        return text;
     }
+    public static String getPeriodFromBirthday(){
+
+        long timeFlowYears= System.currentTimeMillis() / 31536000000L;
+        System.out.println(timeFlowYears);
+        long timeFlowMonths = (System.currentTimeMillis() % 31536000000L) / 241920000;
+        System.out.println(timeFlowMonths);
+        long timeFlowDays = ((System.currentTimeMillis() % 31536000000L) % 241920000) / 86400000;
+        System.out.println(timeFlowDays);
+        return ("years: " + timeFlowYears + " months: " + timeFlowMonths + " days: " + timeFlowDays);
+
+    }
+
 }
